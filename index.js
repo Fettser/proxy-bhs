18ab9d8462ef1b934112668fe4d5e6fa6540ec5b
Fettser <fettser789@gmail.com>
2022-07-20 00:53:45 +0300

    9 commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const PORT = process.env.PORT || 443
+const PORT = process.env.PORT || 8080
 const HOST = process.env.HOST || '0.0.0.0'
 
 const cors_proxy = require('cors-anywhere')
 
 cors_proxy.createServer({
     originWhitelist: ['https://bearheadstudio.ru'],
-    requireHeader: ['origin'],
+    requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(PORT, HOST, () => {
     console.log('proxy running...')
