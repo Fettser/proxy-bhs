33bf7a0846c20cf832a7190d780ba76f6f3c9142
Fettser <fettser789@gmail.com>
2022-12-03 07:34:27 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8000
 const HOST = process.env.HOST || '0.0.0.0'
 
 const cors_proxy = require('cors-anywhere')
 
 cors_proxy.createServer({
-    originWhitelist: ['https://safe-stream-23201.herokuapp.com'],
+    originWhitelist: ['http://localhost:8080'],
     requireHeader: ['origin'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(PORT, HOST, () => {
